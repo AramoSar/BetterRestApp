a75e7cd031962c402fe86039ecc7a20c161acecf
Aram <aramsaribekyan1@gmail.com>
2024-09-23 14:14:16 +0400

    Initial Commit

---
+++ ViewTutorial/ViewTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewTutorialApp.swift
+//  ViewTutorial
+//
+//  Created by Aram on 23.09.24.
+//
+
+import SwiftUI
+
+@main
+struct ViewTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
